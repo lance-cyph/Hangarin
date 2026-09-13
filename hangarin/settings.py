8c845b7cb35f8e5b598336f195c1696a5885a41c
lance-cyph <slance.astig@gmail.com>
2026-09-14 02:12:52 +0800

    Tweaks

--- hangarin/settings.py
+++ hangarin/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1%d@6=)29&3hb-lq9v^v@8)-*6y9q3!))4gd6sex#212j=6&^j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['lancekeith.pythonanywhere.com']
 
 
 # Application definition
@@ -116,6 +116,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # Email
